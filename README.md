17c6d205c459be2d009761534e313ba7fea98554
Jeffrey Clorissaint <93304907+djsainto@users.noreply.github.com>
2022-11-13 17:32:34 -0500

    udpate

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # unemployment_inclass2022
+
+## Setup 
+
+Create and activate a virtual environment:
+
+```sh
+conda create -n unemployment-env python=3.8
+
+conda activate unemplyment-env
+```
+
+## Usage
+
+Run the unemployment report:
+
+```sh
+
+python app/unemployment.py
+
--- app/unemployment.py
+++ app/unemployment.py
@@ -1,1 +1,19 @@
-print ("hello")
+#app\unemployment.py
+import requests
+import json
+from pprint import pprint
+from getpass import getpass
+
+
+API_KEY = getpass("Please input your AlphaVantage API Key: ") 
+
+API_KEY = "demo"
+
+request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
+
+response = requests.get(request_url)
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response))
+pprint(parsed_response)
+
